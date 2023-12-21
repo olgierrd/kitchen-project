a629487da911a2047536674854ee2a5aa4ef6516
Olgierrd <oolgierrd@gmail.com>
2023-12-21 19:16:39 +0100

    add core models + migrated them

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "Dish Type"
+        verbose_name_plural = "Dish Types"
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "Cook"
+        verbose_name_plural = "Cooks"
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name} ({self.username})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    class Meta:
+        verbose_name = "Dish"
+        verbose_name_plural = "Dishes"
+
+    def __str__(self) -> str:
+        return self.name
+
