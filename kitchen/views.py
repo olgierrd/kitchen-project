a8dde07a8b775defb515131a524b5bbdefbdb84e
Olgierrd <oolgierrd@gmail.com>
2023-12-21 19:25:31 +0100

    create empty templates (base, index), added view for home page

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,8 +1,23 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from kitchen.models import Cook, Dish
+
+
 # Create your views here.
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    pass
+    """View function for the home page of the site."""
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_visits = request.session.get("num_visits", 1)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dishes": num_dishes,
+        "num_visits": num_visits,
+    }
+    return render(request, "kitchen/index.html", context=context)
+
