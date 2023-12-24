564e04d35466a7945d2d11bc7d44c468323b6e92
olgierrd <oolgierrd@gmail.com>
2023-12-24 13:59:23 +0100

    added cook detail_view + links from cook list_view

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
-from kitchen.views import index, CookListView, DishListView, IngredientListView
+from kitchen.views import index, CookListView, DishListView, IngredientListView, CookDetailView, DishDetailView, \
+    IngredientDetailView
 
 urlpatterns = [
     path("", index, name="index"),
     path("cooks/", CookListView.as_view(), name="cook-list"),
+    path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
     path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
     path("ingredients/", IngredientListView.as_view(), name="ingredient-list"),
+    path("ingredients/<int:pk>/", IngredientDetailView.as_view(), name="ingredient-detail"),
 ]
 
 app_name = "kitchen"
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from kitchen.models import Cook, Dish, Ingredient
@@ -7,7 +8,7 @@ from kitchen.models import Cook, Dish, Ingredient
 
 # Create your views here.
 
-
+# home page
 def index(request: HttpRequest) -> HttpResponse:
     """View function for the home page of the site."""
     num_cooks = Cook.objects.count()
@@ -23,18 +24,35 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, "kitchen/index.html", context=context)
 
 
+# <-----------Cook Views-------------->
 class CookListView(generic.ListView):
     model = Cook
     paginate_by = 5
 
 
+class CookDetailView(generic.DetailView):
+    model = Cook
+    queryset = Cook.objects.all().prefetch_related("dishes__ingredients")
+
+# <-----------Dish Views-------------->
 class DishListView(generic.ListView):
     model = Dish
     paginate_by = 5
 
 
+class DishDetailView(generic.DetailView):
+    model = Dish
+    success_url = reverse_lazy("kitchen:dish-list")
+
+
+# <-----------Ingredient Views-------------->
 class IngredientListView(generic.ListView):
     model = Ingredient
     template_name = "kitchen/ingredient_list.html"
     context_object_name = "ingredient_list"
     paginate_by = 5
+
+
+class IngredientDetailView(generic.DetailView):
+    model = Ingredient
+    success_url = reverse_lazy("kitchen:ingredient-list")
